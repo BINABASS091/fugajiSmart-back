93bacb176b023a807652d5357d55ca1c64ba5fee
BINABASS091 <bitam10-091@suza.ac.tz>
2025-12-26 15:39:29 +0400

    now

--- config/settings.py
+++ config/settings.py
@@ -205,6 +205,8 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "https://fugaji-smart.vercel.app",
     "https://fugajismart.vercel.app",
     "https://amazing-kuku.vercel.app",
@@ -215,7 +217,7 @@ CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True 
+CSRF_COOKIE_HTTPONLY = False 
 
 
 LANGUAGE_CODE = 'en-us'
